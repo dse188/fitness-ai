e972341d108f5b78a3d28edf60bdec2efd751d78
Derman Cetinalp <derman.cetinalp@gmail.com>
2025-05-24 00:23:48 -0400

    Removed typo Netlify url

--- user-auth-backend/src/app.js
+++ user-auth-backend/src/app.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({ origin: 'https://fitlogdc.netlify.app/', credentials: true })); // Adjust port if needed
+app.use(cors({ origin: 'https://fitlogdc.netlify.app', credentials: true })); // Adjust port if needed
 
 // Routes
 app.use('/api/auth', authRoutes);
